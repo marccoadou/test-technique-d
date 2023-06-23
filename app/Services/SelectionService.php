3b3f5d83a1a434bd06b05cac117d2ee91503d73a
Marc Coadou <63919324+marccoadou@users.noreply.github.com>
2023-06-23 02:13:51 +0200

    Update SelectionService.php
    Rajout d'explications concernant le fonctionnement de l'algorithme de choix aléatoire avec poids

--- app/Services/SelectionService.php
+++ app/Services/SelectionService.php
@@ -6,6 +6,28 @@ use App\Models\Prize;
 
 class SelectionService
 {
+
+    /* Explications :
+        (Tirée du principe de distribution de probabilité pondérée)
+        
+        Ici, chaque élément possède un poids exprimé en décimal
+        On additionne pour trouver le poids total
+        On cherche une variable aléatoire qui match notre décimal
+
+        Dans ce contexte, plus le poids de notre prix est grand,
+        plus il a de chance d'être sélectionné
+        Admettons un exemple tel que : 0.3, 0.1, 0.5 -> poids total 0.9
+        On récupère une variable qui vaudrait : 0.58
+
+        Maintenant, si un item a un poids supérieur à notre random,
+        il fait passer la variable en dessous du négatif, nous donnant
+        ainsi l'item que nous souhaitions
+        Sinon, on continue d'itérer au travers des items en soustrayant
+        pour finalement trouver notre item
+
+        https://gist.github.com/irazasyed/f41f8688a2b3b8f7b6df
+        (qui m'a mis sur la piste)
+    **/
     public static function randomWeightedElement(array $items): Prize
     {
         $totalWeight = 0;
@@ -17,7 +39,6 @@ class SelectionService
 
         $selectedItem = null;
         foreach ($items as $item) {
-            print_r($item);
             $randomNumber -= $item['weight'];
             if ($randomNumber <= 0) {
                 $selectedItem = $item['id'];
